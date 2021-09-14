91eb738fb93f25df33aad223f31975c6a7e9b63d
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2021-09-14 05:45:26 -0500

    Calculadora

---
+++ operacion.js
@@ -0,0 +1,21 @@
+function init(){
+    
+    var reTD = document.getElementsByID("resultadoTD");
+    //var pant2 = document.getElementsByID("pant2");
+    var pant3 = document.getElementsByClassName("pantalla2");
+    document.getElementsByClassName
+
+var resul = document.getElementById("pant2");
+
+}
+
+uno.onclick = function(e){
+    resul.textContent = resul.textContent + "1";
+}
+
+dos.onclick = function(e){
+    result.textContent = result.textContent + "2";
+}
+tres.onclick = function(e){
+    result.textContent = result.textContent + "3";
+}
